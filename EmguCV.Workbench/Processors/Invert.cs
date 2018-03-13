06d56abd7975c40cf6efe2d1c1997846cb237142
Mike Stathopoulos <mstath@gmail.com>
2018-03-13 16:59:59 -0700

    added invert processor

---
+++ EmguCV.Workbench/Processors/Invert.cs
@@ -0,0 +1,13 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EmguCV.Workbench.Processors
+{
+    public class Invert : ImageProcessor
+    {
+        public override void Process(ref Image<Gray, byte> image)
+        {
+            image = image.Not();
+        }
+    }
+}
