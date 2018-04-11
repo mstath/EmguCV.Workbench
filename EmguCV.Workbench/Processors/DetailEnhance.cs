2bc7692e8bd395fa9735ce8ccb50edd47c6e93a9
Mike Stathopoulos <mstath@gmail.com>
2018-04-10 22:30:56 -0700

    added detail enhance processor

---
+++ EmguCV.Workbench/Processors/DetailEnhance.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using EmguCV.Workbench.Util;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+
+namespace EmguCV.Workbench.Processors
+{
+    /// <summary>
+    /// This filter enhances the details of a particular image.
+    /// </summary>
+    /// <seealso cref="EmguCV.Workbench.Processors.ImageProcessor" />
+    public class DetailEnhance : ImageProcessor
+    {
+        private float _sigmaS = 10f;
+        [Category("DetailEnhance")]
+        [PropertyOrder(0)]
+        [DisplayName(@"Sigma S")]
+        public float SigmaS
+        {
+            get { return _sigmaS; }
+            set { Set(ref _sigmaS, value.Clamp(0, 200)); }
+        }
+
+        private float _sigmaR = 0.15f;
+        [Category("DetailEnhance")]
+        [PropertyOrder(1)]
+        [DisplayName(@"Sigma R")]
+        public float SigmaR
+        {
+            get { return _sigmaR; }
+            set { Set(ref _sigmaR, value.Clamp(0, 1)); }
+        }
+
+        public override void Process(ref Image<Bgr, byte> image)
+        {
+            CvInvoke.DetailEnhance(image, image, _sigmaS, _sigmaR);
+        }
+    }
+}
