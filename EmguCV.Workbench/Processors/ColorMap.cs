e2f79a41b07aceacda60d737fbf8aa47245a476f
Mike Stathopoulos <mstath@gmail.com>
2018-04-10 22:19:10 -0700

    added color map processor

---
+++ EmguCV.Workbench/Processors/ColorMap.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+
+namespace EmguCV.Workbench.Processors
+{
+    /// <summary>
+    /// Apply color map to the image.
+    /// </summary>
+    /// <seealso cref="EmguCV.Workbench.Processors.ImageProcessor" />
+    public class ColorMap : ImageProcessor
+    {
+        private ColorMapType _colorMapType = ColorMapType.Autumn;
+        [Category("ApplyColorMap")]
+        [PropertyOrder(0)]
+        [DisplayName(@"Color Map Type")]
+        public ColorMapType ColorMapType
+        {
+            get { return _colorMapType; }
+            set { Set(ref _colorMapType, value); }
+        }
+
+        public override void Process(ref Image<Bgr, byte> image)
+        {
+            CvInvoke.ApplyColorMap(image, image, _colorMapType);
+        }
+    }
+}
